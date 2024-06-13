15e0a09878020880556c779d2ff0a36e1d8ba738
EliasGsN <gabrielsilva20012005@hotmail.com>
2024-06-13 03:30:49 -0300

    Refactoring player movement functionality to the game

--- fuga-das-sombras.lua
+++ fuga-das-sombras.lua
@@ -1,15 +1,16 @@
 -- title:   Fuga das Sombras
 -- desc:    RPG de acao 2D
--- version: 0.1
 -- script:  lua
 jogador = {
     sprite = 32,
     x = 120,
     y = 68
 }
-
-function TIC()
-
+Constantes = {
+    LARGURA_DA_TELA = 240,
+    ALTURA_DA_TELA = 138
+}
+function atualiza()
     -- cima
     if btn(0) then
         jogador.y = jogador.y - 1
@@ -26,21 +27,33 @@ function TIC()
     if btn(3) then
         jogador.x = jogador.x + 1
     end
+end
 
-    cls()
-
+function desenhaMapa()
     map(0, -- posicao x no mapa
     0, -- posicao y no mapa
-    240, -- quanto desenhar x
-    138, -- quanto desenhar y
+    Constantes.ALTURA_DA_TELA, -- quanto desenhar x
+    Constantes.LARGURA_DA_TELA, -- quanto desenhar y
     0, -- em qual ponto colocar o x
     0) -- em qual ponto colocar o y
+end
 
+function desenhaJogador()
     spr(jogador.sprite, jogador.x - 8, jogador.y - 8, 0, -- Cor de fundo
     1, -- escala
     0, -- espelhar
     0, -- rotacionar
     2, -- quantos blocos para direita
     2) -- quantos blocos para baixo
+end
 
+function desenha()
+    cls()
+    desenhaMapa()
+    desenhaJogador()
+end
+
+function TIC()
+    atualiza()
+    desenha()
 end
