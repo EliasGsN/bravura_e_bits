9b1ee268caf335bb793127b3f2da27f4786e269e
EliasGsN <gabrielsilva20012005@hotmail.com>
2024-06-22 02:33:10 -0300

    Refactor player movement functionality, update player sprite animations, and add collision detection

--- fuga-das-sombras.lua
+++ fuga-das-sombras.lua
@@ -3,16 +3,21 @@
 -- script:  lua
 jogador = {
     sprite = 260,
-    x = 120,
+    x = 100,
     y = 68,
     corDeFundo = 2,
-    quadroDeAnimacao = 1
+    quadroDeAnimacao = 1,
+
+    chaves = 0
 }
 
 Constantes = {
     LARGURA_DA_TELA = 240,
     ALTURA_DA_TELA = 138,
-    VELOCIDADE_ANIMACAO_JOGADOR = 0.1
+    VELOCIDADE_ANIMACAO_JOGADOR = 0.1,
+
+    SPRITE_CHAVE = 364,
+    SPRITE_PORTA = 366
 }
 
 objetos = {}
@@ -47,10 +52,8 @@ function moverPara(delta)
         y = jogador.y + 7 + delta.deltaY
     }
 
-    if not (temColisaoComMapa(superiorEsquerdo) or 
-            temColisaoComMapa(superiorDireito) or
-            temColisaoComMapa(inferiorEsquerdo) or 
-            temColisaoComMapa(inferiorDireito)) then
+    if not (temColisaoComMapa(superiorEsquerdo) or temColisaoComMapa(superiorDireito) or
+        temColisaoComMapa(inferiorEsquerdo) or temColisaoComMapa(inferiorDireito)) then
 
         jogador.quadroDeAnimacao = jogador.quadroDeAnimacao + Constantes.VELOCIDADE_ANIMACAO_JOGADOR
         if jogador.quadroDeAnimacao >= 3 then
@@ -77,10 +80,7 @@ function atualiza()
         deltaY = 0
     }}
 
-    AnimacaoJogador = {{256, 258}, 
-                       {260, 262}, 
-                       {264, 266}, 
-                       {268, 270}}
+    AnimacaoJogador = {{256, 258}, {260, 262}, {264, 266}, {268, 270}}
 
     for tecla = 0, 3 do
         if btn(tecla) then
@@ -104,11 +104,7 @@ function desenhaMapa()
 end
 
 function desenhaJogador()
-    spr(jogador.sprite, 
-    jogador.x - 8, 
-    jogador.y - 8, 
-    jogador.corDeFundo, 
-    1, -- escala
+    spr(jogador.sprite, jogador.x - 8, jogador.y - 8, jogador.corDeFundo, 1, -- escala
     0, -- espelhar
     0, -- rotacionar
     2, -- quantos blocos para direita
@@ -117,15 +113,7 @@ end
 
 function desenhaObjetos()
     for indice, objeto in pairs(objetos) do
-        spr(objeto.sprite, 
-            objeto.x - 8, 
-            objeto.y - 8, 
-            objeto.corDeFundo, 
-            1, 
-            0, 
-            0, 
-            2, 
-            2)
+        spr(objeto.sprite, objeto.x - 8, objeto.y - 8, objeto.corDeFundo, 1, 0, 0, 2, 2)
     end
 end
 
@@ -137,6 +125,7 @@ function desenha()
 end
 
 function fazColisaoDoJogadorComAChave(indice)
+    jogador.chaves = jogador.chaves + 1
     table.remove(objetos, indice)
 end
 
@@ -151,19 +140,27 @@ function temColisao(objetoA, objetoB)
     local baixoDeB = objetoB.y + 7
     local cimaDeB = objetoB.y - 8
 
-    if esquerdaDeB > direitaDeA or 
-       direitaDeB < esquerdaDeA or 
-       cimaDeA > baixoDeB or 
-       baixoDeA < cimaDeB then
-       return false
+    if esquerdaDeB > direitaDeA or direitaDeB < esquerdaDeA or cimaDeA > baixoDeB or baixoDeA < cimaDeB then
+        return false
     end
     return true
+end
+
+function fazColisaoDoJogadorComAPorta(indice)
+    if jogador.chaves > 0 then
+        jogador.chaves = jogador.chaves - 1
+        table.remove(objetos, indice)
+    end
 end
 
 function verificaColisaoComObjetos()
     for indice, objeto in pairs(objetos) do
         if temColisao(jogador, objeto) then
-            fazColisaoDoJogadorComAChave(indice)
+            if objeto.sprite == Constantes.SPRITE_CHAVE then
+                fazColisaoDoJogadorComAChave(indice)
+            elseif objeto.sprite == Constantes.SPRITE_PORTA then
+                fazColisaoDoJogadorComAPorta(indice)
+            end
         end
     end
 end
@@ -174,9 +171,19 @@ function TIC()
     desenha()
 end
 
-function criarChave(coluna, linha)
+function criaPorta(coluna, linha)
+    local porta = {
+        sprite = Constantes.SPRITE_PORTA,
+        x = coluna * 8 + 8,
+        y = linha * 8 + 8,
+        corDeFundo = 2
+    }
+    return porta
+end
+
+function criaChave(coluna, linha)
     local chave = {
-        sprite = 364,
+        sprite = Constantes.SPRITE_CHAVE,
         x = coluna * 8 + 8,
         y = linha * 8 + 8,
         corDeFundo = 2
@@ -185,8 +192,11 @@ function criarChave(coluna, linha)
 end
 
 function inicializa()
-    chave = criarChave(3, 3)
+    chave = criaChave(3, 3)
     table.insert(objetos, chave)
+
+    local porta = criaPorta(16, 7)
+    table.insert(objetos, porta)
 end
 
 inicializa()
